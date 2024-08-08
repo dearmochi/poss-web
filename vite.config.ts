6944195b7055ef190d046cc62e4c6b214631e677
dearmochi <1496804+dearmochi@users.noreply.github.com>
2024-08-08 23:01:56 +0900

    ci

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import path from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: process.env.GITHUB_PAGES ? "poss-web" : "/",
+  base: "./",
   plugins: [react()],
   resolve: {
     alias: {
